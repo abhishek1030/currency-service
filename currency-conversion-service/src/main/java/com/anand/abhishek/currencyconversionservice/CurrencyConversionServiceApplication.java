03631dbb17ededdfa939134e53ac5fb7a09c57a3
jon-snow01 <anand007abhishek@gmail.com>
2020-10-27 17:43:04 +0530

    Added Feign as an alternative to RestTemplate

--- currency-conversion-service/src/main/java/com/anand/abhishek/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/anand/abhishek/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,8 +2,10 @@ package com.anand.abhishek.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.anand.abhishek.currencyconversionservice")
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
