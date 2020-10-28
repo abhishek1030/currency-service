5c8f30e6ac563a937cfff2b9ace48a6e0dd377a9
jon-snow01 <anand007abhishek@gmail.com>
2020-10-28 12:04:09 +0530

    Implemented fault tolerance

--- limits-service/src/main/java/com/anand/abhishek/limitsservice/controllers/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/anand/abhishek/limitsservice/controllers/LimitsConfigurationController.java
@@ -2,6 +2,7 @@ package com.anand.abhishek.limitsservice.controllers;
 
 import com.anand.abhishek.limitsservice.bean.LimitsConfiguration;
 import com.anand.abhishek.limitsservice.configurations.Configuration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +16,15 @@ public class LimitsConfigurationController {
     @GetMapping("/limits")
     public LimitsConfiguration retrieveLimitsFromConfiguration(){
         return new LimitsConfiguration(configuration.getMaximum(),configuration.getMinimum());
+    }
+
+    @GetMapping("/fault-tolerance-example")
+    @HystrixCommand(fallbackMethod = "fallBackRetrieveConfiguration")
+    public LimitsConfiguration retrieveConfiguration(){
+        throw new RuntimeException("Not Available");
+    }
+
+    public LimitsConfiguration fallBackRetrieveConfiguration(){
+        return new LimitsConfiguration(99,9);
     }
 }
